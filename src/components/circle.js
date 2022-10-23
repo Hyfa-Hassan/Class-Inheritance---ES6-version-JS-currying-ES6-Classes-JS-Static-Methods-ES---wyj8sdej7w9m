9398cbb0eca1eab68f1f8e3b3bec70117ee4152b
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-23 21:45:01 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,9 +1,11 @@
 // import using require
 const hello = require('./Shape')
-hello.hello1()   // returns hello1
 // declare class
 class Circle extends Shape{
-  
+  constructor(){
+    super(calculateArea);
+  }
 }
 
 // export class using module.exports
+module.exports = Circle

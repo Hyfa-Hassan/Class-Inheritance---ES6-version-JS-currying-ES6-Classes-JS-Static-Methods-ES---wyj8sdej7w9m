6429a75541216704593c07b64bb9c92fecbad3a1
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-23 21:34:43 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,9 +1,18 @@
 // declare class
 class Shape{
-  
+  constructor(color,radius){ //method called automatically
+    this.color = color;
+    this.radius = radius;
+  }
+  get area(){
+    return 3.14*Math.pow(this.radius, 2);
+  }
+  get drawShape(){
+    return 2;
+  }
 }
-
-class Circle extends Shape{
-  
-}
+var circle1 = new Circle(green,5);
+circle1.area();
+circle1.drawShape();
 // export class using module.exports
+export {Shape, Circle}
